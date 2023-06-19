3a6528d79ba0913eae0ae31af064d55e9836a754
Kim Tran <ngockimtranle@gmail.com>
2023-06-19 16:00:39 +0300

    Create basic UI for webpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Details from "./routes/Details";
+import Update from "./routes/Update";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/restaurants/:id" element={<Details />} />
+          <Route path="/restaurants/:id/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
