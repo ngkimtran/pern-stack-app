421721c34f7ae57f55b1a74a236559282bec2991
Kim Tran <ngockimtranle@gmail.com>
2023-06-20 09:24:20 +0300

    Initialize context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Details from "./routes/Details";
 import Update from "./routes/Update";
+import { ResttaurantsContextProvider } from "./context/RestaurantsContext";
 
 const App = () => {
   return (
-    <div className="container">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/restaurants/:id" element={<Details />} />
-          <Route path="/restaurants/:id/update" element={<Update />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ResttaurantsContextProvider>
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id" element={<Details />} />
+            <Route path="/restaurants/:id/update" element={<Update />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ResttaurantsContextProvider>
   );
 };
 
